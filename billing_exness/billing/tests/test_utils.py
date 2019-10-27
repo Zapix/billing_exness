1aad30e4b3071d0fcfbe382136f285e7a2fbbeb7
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2019-10-27 18:04:16 +0300

    Allow make payment from one wallet to another

--- billing_exness/billing/tests/test_utils.py
+++ billing_exness/billing/tests/test_utils.py
@@ -3,12 +3,33 @@ from decimal import Decimal
 
 import pytest
 
-from ..constants import USD, CAD, EUR
-from ..utils import charge
+from ..constants import USD, CAD, EUR, CNY
+from ..utils import charge, make_payment, get_wallet
+from billing_exness.billing.exceptions import NotEnoughMoneyException
 from .factories import WalletFactory, ExchangeRateFactory
 from billing_exness.users.tests.factories import UserFactory
 
 pytestmark = pytest.mark.django_db
+
+
+class TestGetWallet:
+
+    def test_wallet_to_wallet(self):
+        wallet = WalletFactory.create()
+
+        assert wallet.pk == get_wallet(wallet).pk
+
+    def test_user_without_wallet(self):
+        user = UserFactory.create()
+
+        with pytest.raises(ValueError):
+            get_wallet(user)
+
+    def test_user_to_wallet(self):
+        wallet = WalletFactory.create()
+        user = wallet.user
+
+        assert wallet.pk == get_wallet(user).pk
 
 
 class TestCharge:
@@ -50,3 +71,167 @@ class TestCharge:
         )
         updated_wallet = charge(wallet, Decimal("50"), CAD)
         assert updated_wallet.amount == Decimal("125")
+
+
+class TestMakePayment:
+
+    def test_not_enough_money_same_currency(self):
+        from_wallet = WalletFactory.create(
+            amount=Decimal(10),
+            currency=USD,
+        )
+        to_wallet = WalletFactory.create(
+            amount=Decimal(1),
+            currency=USD
+        )
+
+        with pytest.raises(NotEnoughMoneyException):
+            make_payment(
+                from_wallet,
+                to_wallet,
+                Decimal("100"),
+                USD
+            )
+
+    def test_not_enough_money_different_currency(self):
+        ExchangeRateFactory.create(
+            rate=Decimal("2"),
+            currency=EUR
+        )
+        ExchangeRateFactory.create(
+            rate=Decimal("4"),
+            currency=CAD
+        )
+
+        from_wallet = WalletFactory.create(
+            amount=Decimal(10),
+            currency=EUR,
+        )
+        to_wallet = WalletFactory.create(
+            amount=Decimal(1),
+            currency=USD
+        )
+
+        with pytest.raises(NotEnoughMoneyException):
+            make_payment(
+                from_wallet,
+                to_wallet,
+                Decimal("100"),
+                CAD
+            )
+
+    def test_same_currencies(self):
+        ExchangeRateFactory.create(
+            rate=Decimal("2"),
+            currency=EUR
+        )
+        from_wallet = WalletFactory.create(
+            amount=Decimal("10"),
+            currency=EUR,
+        )
+        to_wallet = WalletFactory.create(
+            amount=Decimal("1"),
+            currency=EUR
+        )
+
+        transaction = make_payment(from_wallet, to_wallet, Decimal("4"), EUR)
+
+        assert transaction.amount == Decimal("4")
+        assert transaction.currency == EUR
+
+        from_wallet.refresh_from_db()
+        assert from_wallet.amount == Decimal("6")
+        assert from_wallet.currency == EUR
+
+        to_wallet.refresh_from_db()
+        assert to_wallet.amount == Decimal("5")
+        assert to_wallet.currency == EUR
+
+    def test_from_wallet_same_currency(self):
+        ExchangeRateFactory.create(
+            rate=Decimal("2"),
+            currency=EUR
+        )
+        from_wallet = WalletFactory.create(
+            amount=Decimal("10"),
+            currency=USD,
+        )
+        to_wallet = WalletFactory.create(
+            amount=Decimal("1"),
+            currency=EUR
+        )
+
+        transaction = make_payment(from_wallet, to_wallet, Decimal("3"), USD)
+
+        assert transaction.amount == Decimal("3")
+        assert transaction.currency == USD
+
+        from_wallet.refresh_from_db()
+        assert from_wallet.amount == Decimal("7")
+        assert from_wallet.currency == USD
+
+        assert to_wallet.amount == Decimal("7")
+        assert to_wallet.currency == EUR
+
+    def test_to_wallet_same_currency(self):
+        ExchangeRateFactory.create(
+            rate=Decimal("2"),
+            currency=EUR
+        )
+        from_wallet = WalletFactory.create(
+            amount=Decimal("10"),
+            currency=USD,
+        )
+        to_wallet = WalletFactory.create(
+            amount=Decimal("1"),
+            currency=EUR
+        )
+
+        transaction = make_payment(from_wallet, to_wallet, Decimal("3"), EUR)
+
+        assert transaction.amount == Decimal("3")
+        assert transaction.currency == EUR
+
+        from_wallet.refresh_from_db()
+        assert from_wallet.amount == Decimal("8.5")
+        assert from_wallet.currency == USD
+
+        to_wallet.refresh_from_db()
+        assert to_wallet.amount == Decimal("4")
+        assert to_wallet.currency == EUR
+
+    def test_all_different_currencies(self):
+        ExchangeRateFactory.create(
+            rate=Decimal("2"),
+            currency=EUR
+        )
+        ExchangeRateFactory.create(
+            rate=Decimal("4"),
+            currency=CAD
+        )
+        ExchangeRateFactory.create(
+            rate=Decimal("6"),
+            currency=CNY
+        )
+
+        from_wallet = WalletFactory.create(
+            amount=Decimal("10"),
+            currency=EUR,
+        )
+        to_wallet = WalletFactory.create(
+            amount=Decimal("1"),
+            currency=CAD
+        )
+
+        transaction = make_payment(from_wallet, to_wallet, Decimal("6"), CNY)
+
+        assert transaction.amount == Decimal("6")
+        assert transaction.currency == CNY
+
+        from_wallet.refresh_from_db()
+        assert from_wallet.amount == Decimal("8")
+        assert from_wallet.currency == EUR
+
+        to_wallet.refresh_from_db()
+        assert to_wallet.amount == Decimal("5")
+        assert to_wallet.currency == CAD
--- billing_exness/billing/utils.py
+++ billing_exness/billing/utils.py
@@ -4,12 +4,28 @@ from decimal import Decimal
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractBaseUser
-from django.db  import transaction
+from django.db import transaction
 
-from .models import Wallet, ExchangeRate
-from .constants import CURRENCIES
+from .models import Wallet, ExchangeRate, Transaction
+from .exceptions import NotEnoughMoneyException, check_currency
 
 User = get_user_model()
+
+
+def get_wallet(wallet: Union[AbstractBaseUser, Wallet]) -> Wallet:
+    """
+    Checks has user wallet.
+    Raises:
+        ValueError if user hasn`t got wallet
+    """
+    if isinstance(wallet, User):
+        try:
+            wallet = wallet.wallet
+        except Wallet.DoesNotExist:
+            raise ValueError(f"User {wallet.get_username()} hasn`t got wallet")
+    assert isinstance(wallet, Wallet)
+
+    return wallet
 
 
 @transaction.atomic
@@ -32,18 +48,64 @@ def charge(
     Returns:
         `Wallet` - updated wallet
     """
-    assert currency in CURRENCIES
+    check_currency(currency)
 
-    if isinstance(wallet, User):
-        try:
-            wallet = wallet.wallet
-        except Wallet.DoesNotExist:
-            raise ValueError(f"User {wallet.get_username()} hasn`t got wallet")
-
-    assert isinstance(wallet, Wallet)
+    wallet = get_wallet(wallet)
 
     rate = ExchangeRate.get(currency, wallet.currency)
     wallet.amount += amount * rate
     wallet.save()
 
     return wallet
+
+
+@transaction.atomic
+def make_payment(
+    from_wallet: Union[AbstractBaseUser, Wallet],
+    to_wallet: Union[AbstractBaseUser, Wallet],
+    amount: Decimal,
+    currency: str
+) -> Transaction:
+    """
+    Make payment from `from_wallet` to `to_wallet` in any currencies.
+    Checks that from_wallet has got enough money for processing it.
+    Stores info about transaction in db
+    Params:
+        from_wallet - `Wallet` or `User` that will pay
+        to_wallet - `Wallet` or `User` that will receive payment
+        amount - amount of payment in Decial
+        currency - currency of processing payment
+    Raises:
+        AssertionError - if wrong currency has been passed
+        ValueError - if one of passed users hasn`t got wallet
+        NotEnoughMoneyException - if from_wallet hasn`t got enough money to
+                                  proceed transaction
+    Returns:
+        `Transaction` - object of current transaction
+    """
+    check_currency(currency)
+
+    from_wallet = get_wallet(from_wallet)
+    to_wallet = get_wallet(to_wallet)
+
+    from_rate = ExchangeRate.get(from_wallet.currency, currency)
+    to_rate = ExchangeRate.get(currency, to_wallet.currency)
+
+    if from_rate * from_wallet.amount < amount:
+        username = from_wallet.user.get_username()
+        raise NotEnoughMoneyException(
+            f"{username} hasn`t got {amount} {currency}"
+        )
+
+    from_wallet.amount -= amount / from_rate
+    to_wallet.amount += amount * to_rate
+
+    from_wallet.save()
+    to_wallet.save()
+
+    return Transaction.objects.create(
+        from_wallet=from_wallet,
+        to_wallet=to_wallet,
+        amount=amount,
+        currency=currency
+    )
