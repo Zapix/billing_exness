6d851f26c89bf6e51f571d022a74f976a5856c12
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2019-10-27 20:51:49 +0300

    Add city, country for user model

--- billing_exness/users/models.py
+++ billing_exness/users/models.py
@@ -9,6 +9,8 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    city = CharField(_("City"), blank=True, max_length=255)
+    country = CharField(_("Country"), blank=True, max_length=255)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
